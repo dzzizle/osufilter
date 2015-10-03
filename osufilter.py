344a25efbbcc78c6fc6ee46c36c923747d4f9acd
Ian G <dzzizle@users.noreply.github.com>
2015-10-03 22:26:19 +1000

    redundancies

--- osufilter.py
+++ osufilter.py
@@ -7,7 +7,6 @@ def get_files(dir):
 song_dir = sys.argv[1]
 diffs = ["Easy", "Normal", "Beginner", "Standard", "Taiko", "Oni", "Muzukashii"];
 
-#Change if you want to delete a certain types
 del_maps = True
 del_hitsounds = True
 del_skins = False
@@ -20,16 +19,16 @@ for dir in get_files(song_dir):
     dir_path = song_dir + dir + "/"
     for file in get_files(dir_path):
         if file.endswith(".osu") and del_maps: 
-            difficulty = file.split("[")[1].replace("].osu", "") #for the rare occasion that the song name contains an difficulty name
+            difficulty = file.split("[")[1].replace("].osu", "")
             for diff in diffs:
                 if diff in difficulty:
                     os.remove(dir_path + file)
                     print("Deleted " + dir_path + file)
                     break
-        elif file.endswith(".wav") and del_hitsounds: #only hitsounds are .wav
+        elif file.endswith(".wav") and del_hitsounds: 
             os.remove(dir_path + file)
             print("Deleted " + dir_path + file)
-        elif file.endswith(".png") and del_skins: #backgrounds are USUALLY only .jpg. Use at own risk.
+        elif file.endswith(".png") and del_skins: 
             os.remove(dir_path + file)
             print("Deleted " + dir_path + file)
         elif file.endswith(".jpg") and del_bg: 
